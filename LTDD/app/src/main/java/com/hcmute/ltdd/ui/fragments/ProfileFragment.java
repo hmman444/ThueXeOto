1b8340a32ed5992248d841a97b570ff3ac42661b
Huynh-Minh-Man-04042004 <nsndman0404@gmail.com>
2025-03-23 00:34:52 +0700

    Push UI profile

--- LTDD/app/src/main/java/com/hcmute/ltdd/ui/fragments/ProfileFragment.java
+++ LTDD/app/src/main/java/com/hcmute/ltdd/ui/fragments/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.hcmute.ltdd.ui.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,12 +11,24 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.hcmute.ltdd.R;
+import com.hcmute.ltdd.ui.AccountActivity;
 
 public class ProfileFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        // Lấy tham chiếu đến phần tử "Tài khoản của tôi"
+        View accountLayout = view.findViewById(R.id.account_layout);
+
+        // Bắt sự kiện click để mở AccountActivity
+        accountLayout.setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(), AccountActivity.class);
+            startActivity(intent);
+        });
+
+        return view;
     }
 }
