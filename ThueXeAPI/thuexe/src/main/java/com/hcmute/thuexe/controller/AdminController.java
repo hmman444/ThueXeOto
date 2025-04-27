b3e12890cfef76c218536c564088ca97382b4872
Huynh-Minh-Man-04042004 <nsndman0404@gmail.com>
2025-04-27 22:51:28 +0700

    Delete API before merge main

--- ThueXeAPI/thuexe/src/main/java/com/hcmute/thuexe/controller/AdminController.java
+++
@@ -1,5 +0,0 @@
-package com.hcmute.thuexe.controller;
-
-public class AdminController {
-    
-}
--- ThueXeAPI/thuexe/src/main/java/com/hcmute/thuexe/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.hcmute.thuexe.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.hcmute.thuexe.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
