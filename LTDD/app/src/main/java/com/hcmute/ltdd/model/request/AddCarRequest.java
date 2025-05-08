e7534f4f214862e6dad6baeb5ad4ecea9acfd861
Huynh-Minh-Man-04042004 <nsndman0404@gmail.com>
2025-05-09 01:31:13 +0700

    Push function Register Car with Cloudinary

--- LTDD/app/src/main/java/com/hcmute/ltdd/model/request/AddCarRequest.java
+++ LTDD/app/src/main/java/com/hcmute/ltdd/model/request/AddCarRequest.java
@@ -1,5 +1,7 @@
 package com.hcmute.ltdd.model.request;
 
+import java.util.List;
+
 public class AddCarRequest {
     private String name;
     private String brand;
@@ -10,8 +12,10 @@ public class AddCarRequest {
     private double energyConsumption;
     private String imageUrl;
     private boolean driverRequired;
+    private List<String> features;
 
-    public AddCarRequest(String name, String brand, String description, String gearType, int seats, String fuelType, double energyConsumption, String imageUrl, boolean driverRequired) {
+    public AddCarRequest(String name, String brand, String description, String gearType, int seats, String fuelType,
+                         double energyConsumption, String imageUrl, boolean driverRequired, List<String> features) {
         this.name = name;
         this.brand = brand;
         this.description = description;
@@ -21,15 +25,86 @@ public class AddCarRequest {
         this.energyConsumption = energyConsumption;
         this.imageUrl = imageUrl;
         this.driverRequired = driverRequired;
+        this.features = features;
     }
 
-    public String getName() { return name; }
-    public String getBrand() { return brand; }
-    public String getDescription() { return description; }
-    public String getGearType() { return gearType; }
-    public int getSeats() { return seats; }
-    public String getFuelType() { return fuelType; }
-    public double getEnergyConsumption() { return energyConsumption; }
-    public String getImageUrl() { return imageUrl; }
-    public boolean isDriverRequired() { return driverRequired; }
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getGearType() {
+        return gearType;
+    }
+
+    public void setGearType(String gearType) {
+        this.gearType = gearType;
+    }
+
+    public int getSeats() {
+        return seats;
+    }
+
+    public void setSeats(int seats) {
+        this.seats = seats;
+    }
+
+    public boolean isDriverRequired() {
+        return driverRequired;
+    }
+
+    public void setDriverRequired(boolean driverRequired) {
+        this.driverRequired = driverRequired;
+    }
+
+    public List<String> getFeatures() {
+        return features;
+    }
+
+    public void setFeatures(List<String> features) {
+        this.features = features;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public double getEnergyConsumption() {
+        return energyConsumption;
+    }
+
+    public void setEnergyConsumption(double energyConsumption) {
+        this.energyConsumption = energyConsumption;
+    }
+
+    public String getFuelType() {
+        return fuelType;
+    }
+
+    public void setFuelType(String fuelType) {
+        this.fuelType = fuelType;
+    }
 }
