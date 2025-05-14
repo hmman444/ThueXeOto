6e47e30403924813820b8ef801cbc381e472e725
Huynh-Minh-Man-04042004 <nsndman0404@gmail.com>
2025-05-15 02:35:48 +0700

    Update function EditProfile

--- LTDD/app/src/main/java/com/hcmute/ltdd/model/request/EditProfileRequest.java
+++ LTDD/app/src/main/java/com/hcmute/ltdd/model/request/EditProfileRequest.java
@@ -5,12 +5,14 @@ public class EditProfileRequest {
     private String phone;
     private String birthdate;  // Sử dụng String thay vì Object
     private String gender;
+    private String address;   // Thêm trường address
 
-    public EditProfileRequest(String name, String phone, String birthdate, String gender) {
+    public EditProfileRequest(String name, String phone, String birthdate, String gender, String address) {
         this.name = name;
         this.phone = phone;
         this.birthdate = birthdate;  // birthdate phải là định dạng "yyyy-MM-dd"
         this.gender = gender;
+        this.address = address;
     }
 
     public String getName() {
@@ -27,5 +29,9 @@ public class EditProfileRequest {
 
     public String getGender() {
         return gender;
+    }
+
+    public String getAddress() {
+        return address;
     }
 }
